0d8e5e067f25574df9e87cae1212533ac04ca0ef
meshwpatel <meshwpatel9@gmail.com>
2025-08-29 20:10:19 +0530

    Add files via upload

---
+++ meshw_98(3).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int x,y, multiply;
+    printf("enetr two numbers");
+    scanf("%d %d", &x, &y);
+    multiply = x*y;
+    printf("multiply= %d\n",multiply);
+    return 0;
+}
