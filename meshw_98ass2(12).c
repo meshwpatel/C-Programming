8d536c8974d28ed6ba93d404facca563bd2dbc71
Meshw Patel <meshwpatel9@gmail.com>
2025-09-30 23:01:23 +0530

    Add files via upload

---
+++ meshw_98ass2(12).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter the number of times to print MESHW: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("MESHW\n");
+    }
+
+    return 0;
+}
